fa3f2a921d8410db64eb63f574997dd42f60e1cf
Rubén D. Guerrero N. <rudargeneira@gmail.com>
2025-09-27 19:46:07 -0400

    feat: Add Users module and password hashing

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+
+export const roundsOfHashing = 10;
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  async create(createUserDto: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(
+      createUserDto.password,
+      roundsOfHashing,
+    );
+    createUserDto.password = hashedPassword;
+    return this.prisma.user.create({ data: createUserDto });
   }
 
   findAll() {
